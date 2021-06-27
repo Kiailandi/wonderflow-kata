348ffc33dfc6f195542ff067ed00871fbb7ffc92
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-06-28 01:40:42 +0200

    Update regex to match even if spaces are missing

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const chatParser = (inputString) => {
     /**
      * ^((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)) - date in formato HH:MM:SS 24h ad inizio stringa
-     * ([Cc]ustomer) - type, customer (solo customer essendo mono riga al momento)
-     * ^(((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)) ([Cc]ustomer) : ) - mention
+     * \s* - spazio facoltativo, non è specificato se lo spazio è sempre presente o meno 
+     * ([Cc]ustomer) - type, customer (solo customer essendo mono riga al momento) (iniziale case insensitive - si suppone il resto del nome minuscolo)
+     * (((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer)\s*:\s*) - mention
      * .* - sentence, resto del messaggio fino alla fine
      * 
      * i capturing groups sono data, mention, sentence e type
      */
-    const regexpTokens = /^(((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)) ([Cc]ustomer) : )(.*)$/;
+    const regexpTokens = /^(((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer)\s*:\s*)(.*)$/;
     const match = inputString.match(regexpTokens);
     const resultArray = [{
         date: match[2],
