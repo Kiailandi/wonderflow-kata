080aeb5cae291f2ae910189116692bbbf532e997
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-06-28 01:35:47 +0200

    Add lowercase customer test

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,7 @@
 describe("ChatParser", function() {
     const chatParser = require('../index.js');
     
-    it('should parse a one line string', function () {
+    it('should parse a general one line string', function () {
         const inputString = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
         const expectedOutput = [{
             date: '14:24:32',
@@ -11,4 +11,15 @@ describe("ChatParser", function() {
         }];
         expect(chatParser(inputString)).toEqual(expectedOutput);
     });
-})
+
+    it('should parse a one line string with lower case customer', function () {
+        const inputString = `14:24:32 customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
+        const expectedOutput = [{
+            date: '14:24:32',
+            mention: '14:24:32 customer : ',
+            sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            type: 'customer'
+        }];
+        expect(chatParser(inputString)).toEqual(expectedOutput);
+    });
+});
