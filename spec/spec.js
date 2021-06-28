37c7152c2c641d1cdfd2a9c86b6f752649238396
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-06-28 09:48:27 +0200

    Add test for 2 customer mentions on start

--- spec/spec.js
+++ spec/spec.js
@@ -84,4 +84,31 @@ describe("ChatParser", function() {
         const output = chatParser(inputString);
         expect(output).toEqual(expectedOutput);
     });
+
+    it('should parse a n lines string with 2 customer mentions at the start', function () {
+        const inputString = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend.\n14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text.`;
+        const expectedOutput = [{
+            date: '14:24:32',
+            mention: '14:24:32 Customer : ',
+            sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+            type: 'customer'
+          }, {
+            date: '14:27:00',
+            mention: '14:27:00 Customer : ',
+            sentence: 'Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n',
+            type: 'customer'
+          }, {
+            date: '14:27:47',
+            mention: '14:27:47 Agent : ',
+            sentence: 'Vestibulum tempor diam eu leo molestie eleifend.\n',
+            type: 'agent'
+          }, {
+            date: '14:28:28',
+            mention: '14:28:28 Customer : ',
+            sentence: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+            type: 'customer'
+          }];
+        const output = chatParser(inputString);
+        expect(output).toEqual(expectedOutput);
+    });
 });
