b30a0d612b6019bc6b0c37f5d7efd40b5a750718
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-06-28 11:51:26 +0200

    Update regex to stop at mention instead of date

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const chatParser = (inputString) => {
      * \s* - spazio facoltativo, non è specificato se lo spazio è sempre presente o meno 
      * ([Cc]ustomer|[Aa]gent) - type, customer o agent (iniziale case insensitive - si suppone il resto del nome minuscolo)
      * (((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer|[Aa]gent)\s*:\s*) - mention
-     * ((?:(?!(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)).)*(?:\r\n|\r|\n|$)?) - sentence, resto del messaggio fino a newline o fine riga o data (non contenuta nel gruppo)
+     * ((?:(?!((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer|[Aa]gent)\s*:\s*).)*(?:\r\n|\r|\n|$)?) - sentence, resto del messaggio fino a newline o fine riga o a mention (non contenuta nel gruppo)
      * 
      * i capturing groups sono data, mention, sentence e type
      */
-    const regexpTokens = /(((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer|[Aa]gent)\s*:\s*)((?:(?!(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)).)*(?:\r\n|\r|\n|$)?)/mg;
+    const regexpTokens = /(((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer|[Aa]gent)\s*:\s*)((?:(?!((?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d))\s*([Cc]ustomer|[Aa]gent)\s*:\s*).)*(?:\r\n|\r|\n|$)?)/mg;
     const matches = inputString.matchAll(regexpTokens);
     const resultArray = Array.from(matches, (match) => ({
         date: match[2],
